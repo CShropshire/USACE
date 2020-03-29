37275f458b77c94d6c4b5b329250f8f12fd440fc
CShropshire <shropshirec@gmail.com>
2020-03-29 14:25:13 -0600

    Tied front end to back end

--- front_end.py
+++ front_end.py
@@ -1,5 +1,8 @@
 from tkinter import *
+from tkinter import messagebox
 from tkinter import filedialog
+from controller import trigger
+
 
 # creates welcome window
 welcome = Tk()
@@ -10,14 +13,26 @@ welcome = Tk()
 
 # creates choose file button and defines the openFile command
 def openFile():
-    filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(("CSV files", "*.csv"),("all files", "*.*")))
-    #filename_txt = Label()
+    selectedfile = filedialog.askopenfilename(initialdir="./", title="Select A File", filetypes=(("CSV files", "*.csv"),("all files", "*.*")))
+    fname.delete(0, END)
+    fname.insert(0, selectedfile)
+
+def printtester():
+    print(fname.get())
+
+def submitaction():
+    if (not fname.get()[-4:] == ".csv"):
+        messagebox.showinfo("Error!", "Selected file is not a CSV")
+    else:
+        trigger(fname.get())
+
+
 
 select_file_btn = Button(welcome, text="Select a File", fg='white', bg='gray', command=openFile)
 select_file_btn.place(relx=0.8, rely=0.77, anchor=W)
 
 # creates submit button - currently only closes the window, needs to be updated to actually run the data manipulation function
-submit = Button(welcome, text="Submit", fg='white', bg='gray', width=10, command=welcome.destroy)
+submit = Button(welcome, text="Submit", fg='white', bg='gray', width=10, command=submitaction)
 submit.place(relx=0.4, rely=0.9, anchor=CENTER)
 
 # creates cancel button
@@ -27,12 +42,6 @@ cancel_btn.place(relx=0.6, rely=0.9, anchor=CENTER)
 # creates tool label - ISM CSV Grapher Tool
 tool_lbl = Label(welcome, text="USACE Invasive Species Management CSV Grapher Tool", fg='black', font=("Times New Roman", 16))
 tool_lbl.place(relx=0.5, rely=0.1, anchor=CENTER)
-
-# failed attempt at creating an image widget to display
-#path1 = "C:\Users\lanef\Desktop\1 - St. Johns River, 1968.gif"
-#mg1 = PhotoImage(file=path1)
-#river_picture = Label(window1, image = img1)
-#river_picture.place(relx=0.5, rely=0.5, anchor = CENTER)
 
 # creates description of tool that is split into three labels because i cant figure out text wrapping
 description_lbl1 = Label(welcome, text="The purpose of this tool is to effectively read and graph various CSV files.")
