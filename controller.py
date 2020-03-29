67165bb0442ec8e5c64b04c54e9a6f250c30071d
CShropshire <shropshirec@gmail.com>
2020-03-29 12:40:19 -0600

    Malforned line dealt with

--- controller.py
+++ controller.py
@@ -10,14 +10,15 @@ def line_formatter(input):
     if (len(input) == 3):
         return (input[0], (input[1], float(input[2])))
     else:
-        print("Malforned line item passed to line_formatter")
+        print("Malformed line item passed to line_formatter")
 
 def import_csv(target_file = 'datafiles\LakeO_AmmoniaReports.csv'):
     with open(target_file, newline='') as csvfile:
         active_reader = csv.reader(csvfile, delimiter=',')
         for row in active_reader:
-            formatted = line_formatter(row)
-            insert_data(formatted[0], formatted[1])
+            if (not row[0] == 'Station ID'):
+                formatted = line_formatter(row)
+                insert_data(formatted[0], formatted[1])
 
 def retrieve_stored_data():
     return dataStorage
